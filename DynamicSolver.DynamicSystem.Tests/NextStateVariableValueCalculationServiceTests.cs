65d885c9987e45efd2b0af11fbb9b120ae3f52da
Pavel Borisov <club0174@gmail.com>
2016-04-17 05:16:14 +0300

    #14 Вывод формул для вычисления Xn+1

---
+++ DynamicSolver.DynamicSystem.Tests/NextStateVariableValueCalculationServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using DynamicSolver.Abstractions.Expression;
+using DynamicSolver.Expressions.Parser;
+using NUnit.Framework;
+
+namespace DynamicSolver.DynamicSystem.Tests
+{
+    [TestFixture]
+    public class NextStateVariableValueCalculationServiceTests
+    {
+        private IExpressionParser Parser { get; } = new ExpressionParser();
+
+        private NextStateVariableValueCalculationService Service { get; } = new NextStateVariableValueCalculationService();
+
+        [Test]
+        public void Express_WhenInvalidParameters_ThrowsArgumentException()
+        {
+            var equationSystem = new ExplicitOrdinaryDifferentialEquationSystem(new[]
+            {
+                ExplicitOrdinaryDifferentialEquation.FromStatement(Parser.Parse("x1' = x1")),
+            });
+
+            Assert.That(() => Service.ExpressNextStateVariableValueExpressions(null, "h"), Throws.ArgumentNullException);
+            Assert.That(() => Service.ExpressNextStateVariableValueExpressions(equationSystem, null), Throws.ArgumentException);
+            Assert.That(() => Service.ExpressNextStateVariableValueExpressions(equationSystem, ""), Throws.ArgumentException);
+            Assert.That(() => Service.ExpressNextStateVariableValueExpressions(equationSystem, "  "), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void Express_WhenEquationSystemContainsVariableEqualToStepVariable_ThrowsArgumentException()
+        {
+            var equations = new[]
+            {
+                ExplicitOrdinaryDifferentialEquation.FromStatement(Parser.Parse("x1' = -sin(x2)")),
+                ExplicitOrdinaryDifferentialEquation.FromStatement(Parser.Parse("x2' = cos(x1)"))
+            };
+            var equationSystem = new ExplicitOrdinaryDifferentialEquationSystem(equations);
+
+            Assert.That(() => Service.ExpressNextStateVariableValueExpressions(equationSystem, "x1"), Throws.ArgumentException);            
+            Assert.That(() => Service.ExpressNextStateVariableValueExpressions(equationSystem, "x2"), Throws.ArgumentException);            
+        }
+
+        [Test]
+        public void Express_LoopbackTest_ThrowsArgumentNullException()
+        {
+            var equations = new []
+            {
+                ExplicitOrdinaryDifferentialEquation.FromStatement(Parser.Parse("x1' = -sin(x2)")),
+                ExplicitOrdinaryDifferentialEquation.FromStatement(Parser.Parse("x2' = cos(x1)"))
+            };
+            var equationSystem = new ExplicitOrdinaryDifferentialEquationSystem(equations);
+
+            var actual = Service.ExpressNextStateVariableValueExpressions(equationSystem, "h");
+
+            IDictionary<string, IStatement> expected = new Dictionary<string, IStatement>()
+            {
+                ["x1"] = Parser.Parse("(1-h*(-(cos(x2)*0)))*x1 + (0 - h*(-(cos(x2)*1)))*x2"),
+                ["x2"] = Parser.Parse("(0 - h*(-sin(x1)*1))*x1 + (1 - h*(-sin(x1)*0))*x2")
+            };
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+    }
+}
---
+++ DynamicSolver.DynamicSystem/NextStateVariableValueCalculationService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DynamicSolver.Abstractions.Expression;
+using DynamicSolver.Expressions.Derivation;
+using DynamicSolver.Expressions.Expression;
+using JetBrains.Annotations;
+
+namespace DynamicSolver.DynamicSystem
+{
+    public class NextStateVariableValueCalculationService
+    {
+        [NotNull]
+        public IDictionary<string, IStatement> ExpressNextStateVariableValueExpressions([NotNull] ExplicitOrdinaryDifferentialEquationSystem equationSystem, [NotNull] string stepVariableName)
+        {
+            if (equationSystem == null) throw new ArgumentNullException(nameof(equationSystem));
+            if (string.IsNullOrWhiteSpace(stepVariableName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(stepVariableName));
+
+            if (equationSystem.Equations.Any(e => e.LeadingDerivative.Variable.Name == stepVariableName || e.Function.Analyzer.Variables.Contains(stepVariableName)))
+            {
+                throw new ArgumentException($"Equation system contains variable with name equal to stepVariableName = {stepVariableName}", nameof(stepVariableName));
+            }
+
+            if (equationSystem.Equations.Any(e => e.LeadingDerivative.Order > 1))
+            {
+                throw new ArgumentException("Equation system contains leading derivatives with order greater than 1", nameof(stepVariableName));
+            }
+
+            var jacobian = GetJacobian(equationSystem);
+
+            var result = new Dictionary<string, IStatement>();
+            foreach (var equationDerivatives in jacobian)
+            {
+                var derivativeMatrixRow = equationDerivatives.Value.Select(e => new
+                {
+                    variable = e.Key,
+                    expr = (IExpression) new SubtractBinaryOperator(
+                        new NumericPrimitive(equationDerivatives.Key == e.Key ? "1" : "0"),
+                        new MultiplyBinaryOperator(new VariablePrimitive(stepVariableName), e.Value))
+                });
+
+                IExpression expression = null;
+                foreach (var rowItem in derivativeMatrixRow)
+                {
+                    var item = new MultiplyBinaryOperator(rowItem.expr, new VariablePrimitive(rowItem.variable));
+                    if (expression == null)
+                    {
+                        expression = item;
+                    }
+                    else
+                    {
+                        expression = new AddBinaryOperator(expression, item);
+                    }
+                }
+                result.Add(equationDerivatives.Key, new Statement(expression));
+            }
+
+            return result;
+        }
+
+        private static IDictionary<string, IDictionary<string, IExpression>> GetJacobian(ExplicitOrdinaryDifferentialEquationSystem equationSystem)
+        {
+            var result = new Dictionary<string, IDictionary<string, IExpression>>();
+
+            var derivationService = new SymbolicDerivationService();
+
+            var leadingVariables = equationSystem.Equations.Select(e => e.LeadingDerivative).ToList();
+
+            foreach (var equation in equationSystem.Equations)
+            {
+                var derivativeDictionary = new Dictionary<string, IExpression>();
+
+                foreach (var leading in leadingVariables)
+                {
+                    derivativeDictionary[leading.Variable.Name] = derivationService.GetDerivative(equation.Function, leading.Variable.Name).Expression;
+                }
+
+                result[equation.LeadingDerivative.Variable.Name] = derivativeDictionary;
+            }
+
+            return result;
+        }
+    }
+}
